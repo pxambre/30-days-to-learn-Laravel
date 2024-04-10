09c70af17ae0f6a4b79ec3258fbc221984a63972
Pedro Xambre <pedro.jcx.1993@gmail.com>
2024-04-10 19:01:36 +0100

    first commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot:heading>
+        Contact Us
+    </x-slot:heading>
+    <h1>Hello from the Contact Page</h1>
+</x-layout>
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Models\Job;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/jobs', function () {
+    return view('jobs', [
+        'jobs' => Job::all()
+    ]);
+});
+
+Route::get('/jobs/{job}', function ($id) {
+
+    $job = Job::find($id);
+
+    return view('job', ['job' => $job]);
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
