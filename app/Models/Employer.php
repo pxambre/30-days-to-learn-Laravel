80e2f266d2cea738c4815ad763e4547055308ef0
Pedro Xambre <pedro.jcx.1993@gmail.com>
2024-04-11 16:42:32 +0100

    pivot table job_tag, eager loadings, install laravel debugbar

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Employer extends Model
 {
@@ -12,4 +13,9 @@ class Employer extends Model
     protected $fillable = [
         'name'
     ];
+
+    public function jobs(): HasMany
+    {
+        return $this->hasMany(Job::class, 'employer_id');
+    }
 }
--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Job extends Model
 {
@@ -15,5 +17,15 @@ class Job extends Model
         'title',
         'salary'
     ];
+
+    public function employer(): BelongsTo
+    {
+        return $this->belongsTo(Employer::class);
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class, foreignPivotKey: 'job_listing_id');
+    }
 
 }
